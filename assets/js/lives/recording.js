d2766637abd5c5012abd2cda73c8080360a56483
nanhaoliang <15753157603@163.com>
2017-10-07 13:50:22 +0800

    new 1007

--- assets/js/lives/recording.js
+++ assets/js/lives/recording.js
@@ -1,89 +1,70 @@
 var app = angular.module('records', []);
-app.controller('myRecord', function($scope,$http){
+app.controller('myRecord', function($scope,$http,$window){
 	
 	$scope.shows = true;
 	
-	$scope.liveList = [
-		{
-			id:"111111111111",
-			title:"录播",
-			title1:"2017年会全程回放门票",
-			img:"http://mpic.tiankong.com/caf/a19/cafa19387b3321be67dfb9451051de70/640.jpg",
-			userId:"11111111",
-			userName:"奥巴牛",
-			place:"临沂市兰山区人民医院",
-			price:"199.00",
-			signUpId:"000",
-			signUp:"122",
-			kaiTime:"09-31 18:61-18:62"
-		},
-		{
-			id:"2222222222222",
-			title:"录播",
-			title1:"2017年会全程回放门票2",
-			img:"http://mpic.tiankong.com/caf/a19/cafa19387b3321be67dfb9451051de70/640.jpg",
-			userId:"2222222222",
-			userName:"奥巴牛",
-			place:"临沂市兰山区人民医院",
-			price:"00.00",
-			signUpId:"001",
-			signUp:"222",
-			kaiTime:"09-31 18:61-18:62"
-		},
-		{
-			id:"2222222222222",
-			title:"录播",
-			title1:"2017年会全程回放门票2",
-			img:"http://mpic.tiankong.com/caf/a19/cafa19387b3321be67dfb9451051de70/640.jpg",
-			userId:"2222222222",
-			userName:"奥巴牛",
-			place:"临沂市兰山区人民医院",
-			price:"00.00",
-			signUpId:"001",
-			signUp:"222",
-			kaiTime:"09-31 18:61-18:62"
-		},
-	]
-	$scope.lives1 = $scope.liveList;
+	$scope.liveList = [];
+	$scope.lives1 = [];
 	
-	// 全部
-	$scope.alls = function(){
+
+	// 初始化查询全部接口
+	$http({
+		method: 'GET',
+		url: 'http://v.polyv.net/uc/services/rest?method=getNewList&readtoken=1dfa53b2-ae76-4bfd-8e98-5fd4ed0dc291&pageNum=1'
+	}).then(function successCallback(response) {
+		$scope.liveList = response.data.data;
 		$scope.lives1 = $scope.liveList;
+		console.log($scope.lives1)
+	}, function errorCallback(response) {
+		// 请求失败执行代码
+		alert("刷新失败");
+	});
+		
+	//全部
+	$scope.alls = function(){
+		$http({
+			method: 'GET',
+			url: 'http://v.polyv.net/uc/services/rest?method=getNewList&readtoken=1dfa53b2-ae76-4bfd-8e98-5fd4ed0dc291&pageNum=1'
+		}).then(function successCallback(response) {
+			$scope.liveList = response.data.data;
+			$scope.lives1 = $scope.liveList;
+			console.log($scope.lives1)
+		}, function errorCallback(response) {
+			// 请求失败执行代码
+			alert("刷新失败");
+		});
 	}
-	
+		
 	//付费
 	$scope.pay = function(){
-		var lives = $scope.liveList;
-		$scope.lives1 = [];
-		for (var a = 0; a < lives.length; a++) {
-			if(lives[a].signUpId == "000"){
-				$scope.lives1.push(lives[a]);
-			}
-		}
+		$http({
+			method: 'GET',
+			url: 'http://v.polyv.net/uc/services/rest?method=getNewList&readtoken=1dfa53b2-ae76-4bfd-8e98-5fd4ed0dc291&pageNum=1&catatree=1,1507344254119'
+		}).then(function successCallback(response) {
+			$scope.liveList = response.data.data;
+			$scope.lives1 = $scope.liveList;
+			console.log($scope.lives1)
+		}, function errorCallback(response) {
+			// 请求失败执行代码
+			alert("刷新失败");
+		});
 	}
 	
-	//限免
+	//免费
 	$scope.free = function(){
-		var lives = $scope.liveList;
-		$scope.lives1 = [];
-		for (var a = 0; a < lives.length; a++) {
-			if(lives[a].signUpId == "001"){
-				$scope.lives1.push(lives[a]);
-			}
-		}
+		$http({
+			method: 'GET',
+			url: 'http://v.polyv.net/uc/services/rest?method=getNewList&readtoken=1dfa53b2-ae76-4bfd-8e98-5fd4ed0dc291&pageNum=1&catatree=1,1507344560956'
+		}).then(function successCallback(response) {
+			$scope.liveList = response.data.data;
+			$scope.lives1 = $scope.liveList;
+			console.log($scope.lives1)
+		}, function errorCallback(response) {
+			// 请求失败执行代码
+			alert("刷新失败");
+		});
 	}
 	
-	// 请求参数
-	$http({
-        method: 'GET',
-        url: 'https://www.runoob.com/try/angularjs/data/sites.php'
-    }).then(function successCallback(response) {
-            $scope.names = response.data.sites;
-        }, function errorCallback(response) {
-            // 请求失败执行代码
-			// alert(111)
-    });
-    
     //传递参数
     
 })
